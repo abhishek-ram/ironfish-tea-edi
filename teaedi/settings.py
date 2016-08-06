d6fc2c809b27f1295841578f3e015ca6228dd7b1
Abhishek Ram <abhishek@TEAEDI.CEV.local>
2016-08-06 07:16:49 -0500

    Added admin static files

--- teaedi/settings.py
+++ teaedi/settings.py
@@ -118,9 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'teaedi', 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'teaedi', 'static')
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'teaedi', 'static')
+#]
 # Update login related configurations
 
 LOGIN_URL = '/login/'
