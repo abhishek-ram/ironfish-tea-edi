cdab60da727e984a66d31539337a0f77a964a28b
Abhishek Ram <abhishek8816@gmail.com>
2016-11-02 11:07:44 +0530

    Correct mapping of JSON order to object

--- frontend/teaedi/core/models.py
+++ frontend/teaedi/core/models.py
@@ -2,6 +2,31 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+# class School(models.Model):
+#     isd_name = models.CharField(max_length=100)
+#     isd_code = models.CharField(max_length=30)
+#     district_enrollment = models.CharField(max_length=30)
+#     order = models.IntegerField()
+#     region = models.IntegerField()
+#     sales_id = models.CharField(max_length=10)
+#     rsm = models.CharField(max_length=30)
+#     address_line1 = models.TextField(max_length=100)
+#     address_line2 = models.TextField(max_length=100)
+#     city = models.CharField(max_length=30)
+#     state = models.CharField(max_length=2)
+#     zip = models.CharField(max_length=10)
+#     contact_name = models.CharField(max_length=30)
+#     contact_phone = models.CharField(max_length=30)
+#     contact_fax = models.CharField(max_length=30)
+#     contact_email = models.CharField(max_length=100)
+#     notes = models.TextField(max_length=100)
+#
+#
+# class Salesperson(models.Model):
+#     school = models.ForeignKey(School, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=30)
+
+
 class PurchaseOrder(models.Model):
     ORDER_STATUSES = (
         ('O', 'Open'),
@@ -11,15 +36,27 @@ class PurchaseOrder(models.Model):
     )
 
     order_id = models.CharField(max_length=30, primary_key=True)
-    external_order_id = models.CharField(max_length=30)
-    order_date = models.DateField()
     order_status = models.CharField(
         max_length=2, choices=ORDER_STATUSES, default='O')
-    requisition_id = models.CharField(max_length=30)
-    ship_earliest = models.DateField(null=True)
-    ship_latest = models.DateField(null=True)
-    ship_to_name = models.CharField(max_length=60)
-    ship_to_id = models.CharField(max_length=30)
+    # batch_id = models.CharField(max_length=30)
+    order_date = models.DateField()
+    ship_date = models.DateField()
+    customer_po = models.CharField(max_length=30)
+    contract = models.CharField(max_length=30)
+    isd_name = models.CharField(max_length=100)
+    isd_code = models.CharField(max_length=30)
+    address_line1 = models.TextField(max_length=100)
+    address_line2 = models.TextField(max_length=100, null=True)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    zip = models.CharField(max_length=10)
+    contact_name = models.CharField(max_length=30)
+    contact_phone = models.CharField(max_length=30)
+    contact_fax = models.CharField(max_length=30)
+    contact_email = models.CharField(max_length=100)
+    owed_by_isd = models.DecimalField(
+        max_digits=20, decimal_places=4, null=True)
+    extra = models.DecimalField(max_digits=20, decimal_places=4, null=True)
 
     def __str__(self):
         return self.order_id
@@ -37,17 +74,19 @@ class PurchaseOrderLine(models.Model):
     )
     purchase_order = models.ForeignKey(
         PurchaseOrder, on_delete=models.CASCADE, related_name='lines')
-    line_number = models.CharField(max_length=30)
+    sequence = models.CharField(max_length=30)
     quantity = models.IntegerField()
     quantity_uom = models.CharField(
         max_length=2, choices=QUANTITY_UOM_CHOICES)
     unit_price = models.DecimalField(max_digits=20, decimal_places=4)
     unit_price_code = models.CharField(
         max_length=2, choices=PRICE_CODE_CHOICES)
+    total_price = models.DecimalField(max_digits=20, decimal_places=4)
+    ship_date = models.DateField(null=True)
     isbn = models.CharField(max_length=30)
     student_edition = models.CharField(max_length=30)
     student_edition_cost = models.CharField(max_length=30)
     school_district_owes = models.CharField(max_length=30)
 
     def __str__(self):
-        return '{} {}'.format(self.purchase_order, self.line_number)
+        return '{} {}'.format(self.purchase_order, self.sequence)
--- frontend/teaedi/core/views.py
+++ frontend/teaedi/core/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.authentication import TokenAuthentication
 from .models import PurchaseOrder, PurchaseOrderLine
+from decimal import Decimal
 
 
 @login_required()
@@ -18,31 +19,55 @@ class CreatePurchaseOrder(APIView):
     parser_classes = (JSONParser,)
 
     def post(self, request, format=None):
-        ship_to_name, ship_to_id = None, None
+        po = PurchaseOrder(
+            order_id='TEA%s' % request.data['Header']['OrderNumber'],
+            order_date=request.data['Header']['OrderDate'],
+            customer_po=request.data['Header']['OrderNumber'],
+            contract=request.data['Header']['RequisitionNumber'],
+        )
+        po.ship_date = request.data['Header'].get('RequestedShipLatest') or \
+                       request.data['Header'].get('RequestedShipEarliest')
+
         for address in request.data['Header']['Address']:
             if address['AddressType'] == 'ST':
-                ship_to_name = address['Name']
-                ship_to_id = address['Code']
-        po = PurchaseOrder.objects.create(
-            order_id='TEA%s' % request.data['Header']['OrderNumber'],
-            external_order_id=request.data['Header']['OrderNumber'],
-            order_date=request.data['Header']['OrderDate'],
-            requisition_id=request.data['Header']['RequisitionNumber'],
-            ship_earliest=request.data['Header'].get('RequestedShipEarliest'),
-            ship_latest=request.data['Header'].get('RequestedShipLatest'),
-            ship_to_name=ship_to_name,
-            ship_to_id=ship_to_id
-        )
+                po.isd_name = address['Name']
+                po.isd_code = address['Code']
+                po.address_line1 = address['AddressLine1']
+                if address.get('AddressLine2'):
+                    po.address_line2 = address['AddressLine1']
+                po.city = address['City']
+                po.state = address['StateCode']
+                po.zip = address['PostalCode']
+                po.contact_name = address['ContactName']
+                po.contact_phone = address['ContactPhone']
+                po.contact_email = address['ContactEmail']
+                po.contact_fax = address['ContactFax']
+        po.save()
+
+        order_total, order_total_extra = 0, 0
         for line in request.data['Header']['LineItem']:
+            line_total = Decimal(
+                line['UnitPrice']) * Decimal(line['QuantityOrdered'])
+            order_total += line_total
+            order_total_extra += (
+                line_total + Decimal(line['SchoolDistrictOwes']))
+
             PurchaseOrderLine.objects.create(
                 purchase_order=po,
-                line_number=line['LineNumber'],
+                sequence=line['LineNumber'],
                 quantity=line['QuantityOrdered'],
                 quantity_uom=line['QuantityUOM'],
                 unit_price=line['UnitPrice'],
+                unit_price_code=line['UnitPriceCode'],
+                total_price=line_total,
+                ship_date=po.ship_date,
                 isbn=line['ISBN'],
                 student_edition=line['StudentEdition'],
                 student_edition_cost=line['StudentEditionCost'],
                 school_district_owes=line['SchoolDistrictOwes']
             )
+
+        po.owed_by_isd = order_total
+        po.extra = order_total_extra
+        po.save()
         return Response({'status': 'OK', 'id': po.pk})
